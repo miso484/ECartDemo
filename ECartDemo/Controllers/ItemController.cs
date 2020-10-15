331668f77126f8abff37ec90d18be3cd11e8ce3a
Miso Stamenic <miso.stamenic@wayseven.com>
2020-10-15 11:06:13 +0200

    comment: setup ECart MVC and Layaut, edited Items MVC to display data, added gitignore

---
+++ ECartDemo/Controllers/ItemController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ECartDemo.Models;
+using ECartDemo.ViewModel;
+
+namespace ECartDemo.Controllers
+{
+    public class ItemController : Controller
+    {
+        private ECartDBEntities objECartDbEntities;
+        public ItemController()
+        {
+            objECartDbEntities = new ECartDBEntities();
+        }
+        // GET: Item
+        public ActionResult Index()
+        {
+            ItemViewModel objItemViewModel = new ItemViewModel();
+            objItemViewModel.CategorySelectListItem = (from objCat in objECartDbEntities.Categories
+                                                       select new SelectListItem()
+                                                       {
+                                                           Text = objCat.CategoryName,
+                                                           Value = objCat.CategoryId.ToString(),
+                                                           Selected = true
+                                                       });
+            return View(objItemViewModel);
+        }
+    }
+}
