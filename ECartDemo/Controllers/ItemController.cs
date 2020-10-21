ba1cebf82ee606492e8703ae97a89e34b1afe9db
Miso Stamenic <miso.stamenic@wayseven.com>
2020-10-21 23:38:05 +0200

    comment: finished

--- ECartDemo/Controllers/ItemController.cs
+++ ECartDemo/Controllers/ItemController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using ECartDemo.Models;
 using ECartDemo.ViewModel;
+using System.IO;
 
 namespace ECartDemo.Controllers
 {
@@ -27,6 +28,27 @@ namespace ECartDemo.Controllers
                                                            Selected = true
                                                        });
             return View(objItemViewModel);
+        }
+
+        [HttpPost]
+        public  JsonResult Index(ItemViewModel objItemViewModel)
+        {
+            string NewImage = Guid.NewGuid() + Path.GetExtension(objItemViewModel.ImagePath.FileName);
+            objItemViewModel.ImagePath.SaveAs(Server.MapPath("~/Images/" + NewImage));
+
+            Item objItem = new Item();
+            objItem.ImagePath = "~/Images/" + NewImage;
+            objItem.CategoryId = objItemViewModel.CategoryId;
+            objItem.Description = objItemViewModel.Description;
+            objItem.ItemCode = objItemViewModel.ItemCode;
+            objItem.ItemId = Guid.NewGuid();
+            objItem.ItemName = objItemViewModel.ItemName;
+            objItem.ItemPrice = objItemViewModel.ItemPrice;
+
+            objECartDbEntities.Items.Add(objItem);
+            objECartDbEntities.SaveChanges();
+
+            return Json(new {Success = true, Message = "Item is added Successfully."}, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ ECartDemo/ViewModel/OrderDetailsModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ECartDemo.ViewModel
+{
+    public class OrderDetailsModel
+    {
+
+    }
+}
